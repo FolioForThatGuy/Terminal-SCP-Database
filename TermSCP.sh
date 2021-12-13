2ab77a554f7df1281816272599bc911e93a20c79
FolioForThatGuy <50479881+FolioForThatGuy@users.noreply.github.com>
2021-12-13 16:25:28 +1000

    Update TermSCP.sh
    Removed need for pv

--- TermSCP.sh
+++ TermSCP.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+function slowtype() {
+local text=$(tr -d '\0' < $1)
+local speed=$2
+for ((i=0;i<${#text};i++));
+  do
+      sleep 0.0$(((RANDOM%$speed)+0))
+      echo -n "${text:i:1}";
+done
+}
 function trap_ctrlc ()
 {
 	echo ""
@@ -37,7 +46,7 @@ fi
 if [[ $1 == -s ]] || [[ $1 == --settings ]]; then
 	if  [ -z $2 ]; then
 		echo ""
-		echo "		 $0 $1 speed 		Sets the speed text is written to the terminal"
+		echo "		 $0 $1 speed 		Sets the speed text is written to the screen (lower is faster)"
 		echo "		 $0 $1 offline		Changes storage settings"
 		echo ""
 		exit
@@ -45,7 +54,7 @@ if [[ $1 == -s ]] || [[ $1 == --settings ]]; then
 		read -p "Do you want to store SCP documents offline? [true or false] " write
 		case $write in
 		"true" | "T" | "t" | "True")
-		echo "Offline set to true. Now when you open scp documents they will be saved to your drive."
+		echo "Offline set to true. Now when you open scp documents they will be saved to your hard drive."
 		cat <<< $(jq ".offline = true" $FILE/settings) >  $FILE/settings
 		;;
 		"false" | "f" | "F" | "False")
@@ -65,24 +74,24 @@ if [[ $1 == -s ]] || [[ $1 == --settings ]]; then
 		exit
 	fi
 fi
-echo "Welcome back $USER, to the SCP database"
-echo
+echo -e "Welcome back $USER, to the SCP database"
+
 if ! [ -z $1 ]; then
 	SCPNo=$1
 else
-	read -p "What entry would you like to access?: " SCPNo
+	read -p "What SCP would you like?: " SCPNo
 fi
 if [ $offline == true ]; then
 	if [ -f "$FILE/offline/$SCPNo" ]; then
 		echo "   Item #: SCP-$SCPNo"
-		cat $FILE/offline/$SCPNo | pv -qL $speed
+		slowtype $FILE/offline/$SCPNo $speed
 	else
 		echo "   Item #: SCP-$SCPNo"
 	  lynx -dump -nolist http://www.scpwiki.com/scp-$SCPNo > $FILE/tmp/$SCPNo
 		cat  $FILE/tmp/$SCPNo | grep -izoP '(?<=Item #: SCP-'$SCPNo')(?s).*(?=« SCP-)'  > $FILE/offline/$SCPNo
 		if [ -s $FILE/offline/$SCPNo ]; then
 			stty -echo
-			cat $FILE/offline/$SCPNo | pv -qL $speed
+			slowtype $FILE/offline/$SCPNo $speed
 		else
 			rm $FILE/offline/$SCPNo
 			cat  $FILE/tmp/$SCPNo | grep -izoP '(?<=SCP-'$SCPNo')(?s).*(?=« SCP-)'  > $FILE/offline/$SCPNo
@@ -90,14 +99,14 @@ if [ $offline == true ]; then
 				rm $FILE/offline/$SCPNo
 				cat  $FILE/tmp/$SCPNo | grep -izoP '(?<=SCP-'$SCPNo')(?s).*(?=page revision)'  > $FILE/offline/$SCPNo
 					if  [ -s $FILE/offline/$SCPNo ]; then
-						cat $FILE/offline/$SCPNo | pv -qL $speed
+						slowtype $FILE/offline/$SCPNo
 						
 						else
 							echo "Problem connecting to database, please try again later."
 							rm $FILE/tmp/$SCPNo $FILE/offline/$SCPNo
 						fi
 			else
-				cat $FILE/offline/$SCPNo | pv -qL $speed
+				slowtype $FILE/offline/$SCPNo $speed
 				rm $FILE/tmp/$SCPNo
 					fi
 		fi
@@ -110,6 +119,6 @@ else
 	cat  $FILE/tmp/$SCPNo | grep -izoP '(?<=Item #: SCP-'$SCPNo')(?s).*(?=« SCP-)' >"$FILE/tmp/$SCPNo)";
 	rm  $FILE/tmp/$SCPNo
 	stty -echo
-  cat  "$FILE/tmp/$SCPNo)"  | pv -qL $speed
+ 	slowtype $FILE/offline/$SCPNo $speed
 	stty echo
 fi
